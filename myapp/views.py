b94dd6fcceb75acb454bce3ee9b51d1cf22c8d5b
Arshad vkb <arshadvkb118@gmail.com>
2025-03-14 00:45:05 -0700

    Remove date filter from user_view_tracked_diet and user_view_workout functions

--- myapp/views.py
+++ myapp/views.py
@@ -706,7 +706,7 @@ def user_view_tracked_diet(req):
     if not uid:
         return JsonResponse({"status": "error", "message": "LID is missing"}, status=400)
 
-    obj = Diet.objects.filter(user__LOGIN_id=uid,date=datetime.now().today())
+    obj = Diet.objects.filter(user__LOGIN_id=uid)
     a = []
 
     # Prepare the response data
@@ -729,7 +729,7 @@ def user_view_workout(req):
     if not uid:
         return JsonResponse({"status": "error", "message": "LID is missing"}, status=400)
 
-    obj = Workout.objects.filter(user__LOGIN_id=uid,date=datetime.now().today())
+    obj = Workout.objects.filter(user__LOGIN_id=uid)
     a = []
 
     # Prepare the response data
